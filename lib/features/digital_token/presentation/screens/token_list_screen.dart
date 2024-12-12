9797c109bd3376856b0dc004ba0367f9a40a1d28
Wichasin <wichasin.s@gmail.com>
2024-12-12 18:03:56 +0700

    fix tab bar problem :stateless to stateful

--- lib/features/digital_token/presentation/screens/token_list_screen.dart
+++ lib/features/digital_token/presentation/screens/token_list_screen.dart
@@ -2,27 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:versa_app_tutorial_cleanarch/features/digital_token/presentation/providers/token_provider.dart';
 import 'package:versa_app_tutorial_cleanarch/features/home/presentation/widgets/bottom_navbar_bar.dart';
-import 'package:versa_app_tutorial_cleanarch/shared/theme/test_styles.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TokenListScreen extends ConsumerWidget {
+class TokenListScreen extends ConsumerStatefulWidget {
   const TokenListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, ref) {
-    final selectedTabIndex = ref.watch(selectedTabIndexProvider);
+  _TokenListScreenState createState() => _TokenListScreenState();
+}
+
+class _TokenListScreenState extends ConsumerState<TokenListScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int selectIndexTab = ref.watch(selectedTabIndexProvider);
 
     final List<Widget> tabs = [
       Center(
           child: Text('โทเคนที่เปิดจองอยู่',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold))),
+              style: GoogleFonts.prompt(
+                  fontSize: 18.0, fontWeight: FontWeight.bold))),
       Center(
           child: Text('โทเคนที่ใกล้เปิดจอง',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold))),
+              style: GoogleFonts.prompt(
+                  fontSize: 18.0, fontWeight: FontWeight.bold))),
       Center(
-          child: Text('โทเคนเพื่อการลงทุน',
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold))),
+          child: Text('โทเคนที่ปิดจองแล้ว',
+              style: GoogleFonts.prompt(
+                  fontSize: 18.0, fontWeight: FontWeight.bold))),
     ];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -36,26 +59,62 @@ class TokenListScreen extends ConsumerWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'This is Google Fonts',
-              style: GoogleFonts.kanit(),
-            ),
-            // Text("รายการโทเคน", d
-            //     style: GoogleFonts.prompt(
-            //       textStyle: TextStyle(
-            //         fontSize: 20.0,
-
-            //       ),
-            //     )),
+            Text("รายการโทเคน",
+                style: GoogleFonts.prompt(
+                  textStyle: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )),
             Text(
               'ชำระเงินก่อนจะได้จัดสรรก่อน',
-              style: TextStyle(
-                fontSize: 18,
-                color: Color.fromARGB(255, 112, 112, 112),
-                fontWeight: FontWeight.normal,
+              style: GoogleFonts.prompt(
+                textStyle: TextStyle(
+                  fontSize: 18.0,
+                  color: Color.fromARGB(255, 112, 112, 112),
+                  fontWeight: FontWeight.normal,
+                ),
               ),
             ),
-            SizedBox(width: 16),
+            SizedBox(height: 16),
+            // ใช้ SingleChildScrollView เมื่อมีจำนวนแท็บเยอะเกินไป
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal, // ทำให้ TabBar เลื่อนได้
+              child: TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                dividerColor: Colors.transparent,
+                labelColor: Colors.black, // สีข้อความในแท็บที่ถูกเลือก
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: Colors.orange,
+                indicator: UnderlineTabIndicator(
+                    borderSide: BorderSide(width: 5.0,color: Colors.orange),
+                    insets: EdgeInsets.symmetric(horizontal: 50.0)),
+                labelStyle: GoogleFonts.prompt(
+                  textStyle: TextStyle(fontSize: 18.0),
+                ),
+                onTap: (index) {
+                  ref.read(selectedTabIndexProvider.notifier).changeTab(index);
+                  print("current index : $selectIndexTab");
+                },
+                tabs: [
+                  Tab(text: 'โทเคนที่เปิดจองอยู่'),
+                  Tab(text: 'โทเคนที่ใกล้เปิดจอง'),
+                  Tab(text: 'โทเคนที่ปิดจองแล้ว'),
+                ],
+              ),
+            ),
+            // ใช้ TabBarView ที่ให้เลือกแท็บตาม selectIndexTab
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  tabs[0],
+                  tabs[1],
+                  tabs[2],
+                ],
+              ),
+            ),
           ],
         ),
       ),
